f8e8999325e1fe3f1e9456962e0cb4f03416e483
245752021610137 <phongcachnguoichoi06@gmail.com>
2025-12-08 19:50:53 +0700

    Add files via upload

---
+++ bài 5/cau5.4.py
@@ -0,0 +1,9 @@
+print ("Hoàng Văn Trường\nMSV:245752021610137");
+import numpy as np
+
+x = np.arange(12, 39) 
+print("Mảng ban đầu:", x)
+
+# Đảo ngược mảng bằng slicing [::-1]
+x_reversed = x[::-1]
+print("Mảng đảo ngược:", x_reversed)
